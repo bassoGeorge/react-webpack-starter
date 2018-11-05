61192961811253c8ea837831ec3adea0106714bf
Anish George <Anish_George-GGN@external.mckinsey.com>
2018-11-05 16:15:46 +0530

    Get unit tests with mocha+chai+jsom in place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Heading = ({text}) => <h1>{text}</h1>
+import {Heading} from './heading'
 
 const wrapper = document.getElementById("app")
 wrapper && ReactDOM.render(<Heading text="Hello World!"/>, wrapper)
